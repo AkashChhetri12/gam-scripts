cd99a55a4b280844d8a1bbf2be926090b832c6ec
Akash Chhetri <chhetriakash2616@gmail.com>
2024-01-17 23:50:21 +0530

    updated csv line and field check

--- create-private-folders/create-folder.sh
+++ create-private-folders/create-folder.sh
@@ -8,6 +8,21 @@ do
   # remove leading and trailing spaces
   username=$(echo $username | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
   userid=$(echo $userid | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+
+  # check empty username and userid
+
+  if [ -z "$username" ] && [ -z "$userid" ];
+  then
+    continue
+  fi
+
+  if [ -z "$username" ] || [ -z "$userid" ];
+  then
+    echo "username or userid is empty"
+    continue
+  fi
+
+
   # create folder for users
   folder_id=$(gam user $userid create drivefile drivefilename $DRIVE_FOLDER_NAME mimetype gfolder returnidonly)
   echo "Created $DRIVE_FOLDER_NAME (file_id: $folder_id) folder for $username ($userid)" 
