4658cc47c1c534dbf9804383ab70993cedbf1a73
Akash Chhetri <chhetriakash2616@gmail.com>
2024-01-17 23:02:27 +0530

    Update create-folder.sh

--- create-private-folders/create-folder.sh
+++ create-private-folders/create-folder.sh
@@ -5,6 +5,10 @@ CSV_FILE="user.csv"
 
 sed 1d "$CSV_FILE" | while IFS=, read -r username userid;
 do
+  # remove leading and trailing spaces
+  username=$(echo $username | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+  userid=$(echo $userid | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+  # create folder for users
   folder_id=$(gam user $userid create drivefile drivefilename $DRIVE_FOLDER_NAME mimetype gfolder returnidonly)
   echo "Created $DRIVE_FOLDER_NAME (file_id: $folder_id) folder for $username ($userid)" 
 done
